37fb6f58afecfbe6a0edab44fdc4026b24a336f3
arf-01 <adhroba@gmail.com>
2024-08-28 15:56:59 +0600

    ckeditor working correctly ,database connected

---
+++ WebForm3.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm3.aspx.cs" Inherits="MyBlog.WebForm3" ValidateRequest="false" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CKEditor 5 Integration</title>
+
+    <script src="https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js"></script>
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h3>Add a new blog...</h3>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+     <asp:HiddenField ID="HiddenField1" runat="server" />
+
+    <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+     <script type="text/javascript">
+         ClassicEditor
+             .create(document.querySelector('#<%= TextBox1.ClientID %>'))
+            .then(editor => {
+            
+                editor.model.document.on('change:data', () => {
+                    document.getElementById('<%= HiddenField1.ClientID %>').value = editor.getData();
+                });
+            })
+             .catch(error => {
+                 console.error(error);
+             });
+    </script>
+         
+    
+</asp:Content>
---
+++ WebForm3.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyBlog
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+
+        string strcon = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strcon = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(strcon))
+                {
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Table_1 ([post]) VALUES (@post);", con);
+                    cmd.Parameters.AddWithValue("@post", HiddenField1.Value.Trim());
+
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                }
+
+
+                TextBox1.Text = "";
+                Response.Write("<script>alert('Blog post added successfully!');</script>");
+            }
+            catch (Exception ex)
+            {
+               
+                Response.Write("<script>alert('Error: " + ex.Message + "');</script>");
+            }
+        }
+
+    }
+}
